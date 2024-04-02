ad5c9ef436db4c400907e9e2f0d9851e55eea47b
Mohamad <mohamad.elshimy.ext@nokia.com>
2024-04-02 13:09:32 +0200

    adding "./" in image src for product learn more to make it relative path

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HJUUD3ZS.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-273NSDJU.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
