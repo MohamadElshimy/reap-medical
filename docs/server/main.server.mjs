5370d2a5a90032c5b6378fce8538c454e03ac833
Mohamad <mohamad.elshimy.ext@nokia.com>
2024-04-02 13:05:16 +0200

    adding "/" in background-image url to fix hidden background bug

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-6LG6L26Y.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-HJUUD3ZS.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
