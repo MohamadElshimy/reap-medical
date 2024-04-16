0f9cbd8780334c88a7682d245f5686416addd4d1
Mohamad <mohamad.elshimy.ext@nokia.com>
2024-04-16 11:57:25 +0200

    new build including smaller screens enhancements

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-273NSDJU.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-OGLJRG3H.mjs";import"./chunk-MHH64SYL.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
